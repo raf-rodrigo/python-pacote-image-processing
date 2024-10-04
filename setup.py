bcfe0e28c5ba26a8dfdce94f2805d06a9ad4b6fe
Rafael Rodrigo <rafrodrigo@gmail.com>
2024-10-04 10:04:04 -0300

    criado pastas de processing e utils juntos com seus arquivos

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setup import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="image_processing_rafael_rodrigo",
+    version="0.0.1",
+    author="Rafael Rodrigo",
+    email="raf_rodrigo@gmail.com",
+    description="Package image processing",
+    long_description=page_description,
+    long_description_content_tupe="text/markdown",
+    url="git@github.com:raf-rodrigo/python-pacote-image-processing.git",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires=">=3.4",
+)

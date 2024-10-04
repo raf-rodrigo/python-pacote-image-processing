ba430a2b3bcb16f062405f5a3a4d36a9fcdc7b9c
Rafael Rodrigo <rafrodrigo@gmail.com>
2024-10-04 10:40:03 -0300

    arquivos finais

--- setup.py
+++ setup.py
@@ -8,14 +8,12 @@ with open("requirements.txt") as f:
 
 setup(
     name="image_processing_rafael_rodrigo",
-    version="0.0.1",
+    version="0.0.2",
     author="Rafael Rodrigo",
     email="raf_rodrigo@gmail.com",
     description="Package image processing",
     long_description=page_description,
     long_description_content_tupe="text/markdown",
-    url="git@github.com:raf-rodrigo/python-pacote-image-processing.git",
-    packages=find_packages(),
-    install_requires=requirements,
+    url="https://github.com/raf-rodrigo/python-pacote-image-processing.git",
     python_requires=">=3.4",
 )
